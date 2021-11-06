d8f5a01835e6f93ed2e353740f64f3789192a2de
Niklas Mohrin <dev@niklasmohrin.de>
2021-11-07 00:26:38 +0100

    Fix #6

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,16 @@
 class AuthorsController < ApplicationController
-  def new
+  def new; end
+
+  def create
+    @author = Author.new(author_params)
+    @author.save
+    redirect_to root_path, notice: 'Success!'
+  end
+
+  private
+
+  # see https://edgeapi.rubyonrails.org/classes/ActionController/StrongParameters.html
+  def author_params
+    params.require(:author).permit(:first_name, :last_name, :homepage)
   end
 end
