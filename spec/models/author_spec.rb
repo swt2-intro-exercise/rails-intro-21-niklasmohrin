bd6bf7a7ca57a93f37cbdc73107809b627de7e6c
Niklas Mohrin <dev@niklasmohrin.de>
2021-11-07 00:15:14 +0100

    Fix #5

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -4,5 +4,6 @@ describe 'Author model', type: :model do
   it 'should set the instance attributes in the constructor' do
     author = Author.new(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')
     expect(author.first_name).to eq('Alan')
+    expect(author.name).to eq('Alan Turing')
   end
 end
