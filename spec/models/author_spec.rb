e13a3fc2eae600d9f8a9b13a957755387db4191c
Niklas Mohrin <dev@niklasmohrin.de>
2021-11-09 00:04:30 +0100

    Fix #9

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -6,4 +6,9 @@ describe 'Author model', type: :model do
     expect(author.first_name).to eq('Alan')
     expect(author.name).to eq('Alan Turing')
   end
+
+  it 'should fail when no last name is provided' do
+    author = Author.new(first_name: 'Alan', last_name: '', homepage: 'example.com')
+    expect(author).to_not be_valid
+  end
 end
