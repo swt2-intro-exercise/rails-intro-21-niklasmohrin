7acee4eb2a3da26bbc611ac9a6ff507f9cecca7b
Niklas Mohrin <dev@niklasmohrin.de>
2021-11-12 11:35:39 +0100

    Fix #16

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,2 +1,5 @@
 class Paper < ApplicationRecord
+  validates :title, :venue, :year, presence: true
+  validates :title, length: { minimum: 1 }
+  validates :year, numericality: { only_integer: true }
 end
